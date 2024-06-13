8adb23eeb70a1ba21f2d1b49beb54ce0780a754b
Bhavesh Jadhav <92859197+Bhaveshj008@users.noreply.github.com>
2024-06-13 23:35:13 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory here
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
