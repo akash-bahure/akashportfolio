4709edb4e7010b787b7dd17e10b4562debb92ab3
Bhavesh Jadhav <92859197+Bhaveshj008@users.noreply.github.com>
2024-06-14 00:17:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this matches the actual directory name where Vercel expects outputs
+    outDir: 'dist', 
   },
   server: {
     proxy: {
